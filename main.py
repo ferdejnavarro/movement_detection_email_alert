9727bdf98431b7cb89b5cfc1338260d289347e90
ferdejnavarro <ferdejnavarro@gmail.com>
2024-05-17 14:04:39 -0600

    Movement detection

--- main.py
+++ main.py
@@ -3,9 +3,27 @@ import time
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
+first_frame = None
 
 while True:
     check, frame = video.read()
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gauss_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+    if first_frame is None:
+        first_frame = gauss_frame
+
+    delta_frame = cv2.absdiff(first_frame, gauss_frame)
+    threshold_frame = cv2.threshold(delta_frame, 70, 255, cv2.THRESH_BINARY)[1]
+    dilated_frame = cv2.dilate(threshold_frame, None, iterations=2)
+    contours, check = cv2.findContours(dilated_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 10000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
+
     cv2.imshow("Movement detection", frame)
 
     key = cv2.waitKey(1)
